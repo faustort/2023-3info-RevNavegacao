43916b96650c89ec4adc2c4afc8be7700ff1a673
Your Name <you@example.com>
2023-03-27 16:00:17 -0300

    login

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,15 +1,56 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import { Image, View } from "react-native";
-import { TextInput } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
+import { auth } from "../config/firebase";
 import { styles } from "../utils/styles";
 
 export default function LoginScreen() {
-
-  
-  
-
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
   // função para lidar com o login do Usuário
-  function handleLogin() {}
+  function handleLogin() {
+    signInWithEmailAndPassword(auth, email, senha)
+      .then((userCredencial) => {
+        console.log("Usuário logado com sucesso!");
+        navigation.navigate("HomeScreen");
+      })
+      .catch((error) => {
+        console.log("Erro ao criar usuário", error);
+        //código de erro
+        const errorCode = error.code;
+        if (email === "" || senha === "") {
+          console.log("Preencha todos os campos");
+          return;
+        }
+        if (senha.length < 6) {
+          console.log("A senha deve ter no mínimo 6 caracteres");
+          return;
+        }
+        if (!email.includes("@")) {
+          console.log("E-mail inválido");
+          return;
+        }
+        if (!email.includes(".")) {
+          console.log("E-mail inválido");
+          return;
+        }
+        if (email.includes(" ")) {
+          console.log("E-mail inválido");
+          return;
+        }
+        if (errorCode === "auth/invalid-email") {
+          console.log("E-mail inválido");
+        }
+        if (errorCode === "auth/user-not-found") {
+          console.log("Usuário não encontrado");
+        }
+        if (errorCode === "auth/wrong-password") {
+          console.log("Senha incorreta");
+        }
+      });
+  }
 
   return (
     <View style={styles.container}>
@@ -18,12 +59,20 @@ export default function LoginScreen() {
           source={{ uri: "https://picsum.photos/200" }}
           style={styles.imgRedonda}
         />
-        <TextInput label="Email" placeholder="Digite seu email" />
+        <TextInput
+          label="Email"
+          placeholder="Digite seu email"
+          onChangeText={setEmail}
+          value={email}
+        />
         <TextInput
           label={"Senha"}
           placeholder={"Digite sua senha"}
+          onChangeText={setSenha}
+          value={senha}
           secureTextEntry={true} // faz com que o texto pareça ser uma senha
         />
+        <Button onPress={handleLogin}>Logar</Button>
       </View>
     </View>
   );
