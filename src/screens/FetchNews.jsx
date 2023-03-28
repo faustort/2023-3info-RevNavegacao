99694a661a3c6cab16610e839b3dd2f3b9fabbb7
Your Name <you@example.com>
2023-03-28 13:48:21 -0300

    aula 28

--- src/screens/FetchNews.jsx
+++ src/screens/FetchNews.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import { styles } from "../utils/styles";
 
 export default function FetchNews() {
+  const [data, setData] = useState([]);
+
   function fetchNewsData() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-      .catch((error) => console.error(error));
+      .then((response) => response.json()) // transforma a informação em JSON
+      .then((json) => console.log(json)) // aqui temos o resultado em JSON
+      .catch((error) => console.error(error)); // aqui pegamos o erro
   }
   useEffect(() => {
     fetchNewsData();
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Fetch News</Text>
     </View>
   );
