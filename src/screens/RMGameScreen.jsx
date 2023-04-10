97ec95920557d44e2419bb2a340ddbef43df35fd
Your Name <you@example.com>
2023-04-10 16:32:42 -0300

    aual jogo

--- src/screens/RMGameScreen.jsx
+++ src/screens/RMGameScreen.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 import { View } from "react-native";
+import { Button } from "react-native-paper";
+import { styles } from "../utils/styles";
 
 export default function RMGameScreen() {
     const [personagem, setPersonagem] = useState(null);
+    const [personagens, setPersonagens] = useState(0);
+    const [totalPersonagens, setTotalPersonagens] = useState(0);
 
     function buscaPersonagemAleatorio() {
         fetch('https://rickandmortyapi.com/api/character')
             .then((response) => response.json())
             .then((json) => {
-                console.log(json.results);
+                setTotalPersonagens(json.info.count);
+                setPersonagens(json.results);
+
+                const indiceAleatorio = Math.floor(Math.random() * totalPersonagens);
+                setPersonagem(personagens[indiceAleatorio])
             })
             .catch((error) => {
                 console.error(error);
             });
 
     }
-
-
     return (
-        <View>
-
+        <View style={styles.container}>
+            <Text>Personagem: {personagem?.name}</Text>
+            <Button onPress={buscaPersonagemAleatorio} > Buscar Personagem </Button>
         </View>
     )
 
