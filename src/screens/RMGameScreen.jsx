603aa338373976a8cdf972e62dda56727d4dc402
Your Name <you@example.com>
2023-04-10 16:45:05 -0300

    total

--- src/screens/RMGameScreen.jsx
+++ src/screens/RMGameScreen.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import { styles } from "../utils/styles";
 
 export default function RMGameScreen() {
     const [personagem, setPersonagem] = useState(null);
-    const [personagens, setPersonagens] = useState(0);
+    const [personagens, setPersonagens] = useState([]);
     const [totalPersonagens, setTotalPersonagens] = useState(0);
 
+
+    useEffect(() => {
+        retornaTotalDePesonagens();
+    }, [])
+
     function buscaPersonagemAleatorio() {
+        fetch('https://rickandmortyapi.com/api/character/' + retornaIndiceAleatorio())
+            .then((response) => response.json())
+            .then((json) => {
+                setPersonagem(json);
+            })
+    }
+
+    function retornaTotalDePesonagens() {
         fetch('https://rickandmortyapi.com/api/character')
             .then((response) => response.json())
             .then((json) => {
                 setTotalPersonagens(json.info.count);
-                setPersonagens(json.results);
-
-                const indiceAleatorio = Math.floor(Math.random() * totalPersonagens);
-                setPersonagem(personagens[indiceAleatorio])
-            })
-            .catch((error) => {
-                console.error(error);
             });
-
     }
+
+    function retornaIndiceAleatorio() {
+        return Math.floor(Math.random() * totalPersonagens);
+    }
+
     return (
         <View style={styles.container}>
             <Text>Personagem: {personagem?.name}</Text>
