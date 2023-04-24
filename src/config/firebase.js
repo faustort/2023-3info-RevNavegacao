4e298c8324b51acbbd22dc9e2d44f96cc3f4eb12
Fausto <fausto.toloi@prof.sc.senac.br>
2023-04-24 16:29:10 -0300

    fix firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyAL8e7CY_dWHdbc_7bG86oK_IT-tYA5ZI8",
   authDomain: "aulafirebase-9613a.firebaseapp.com",
@@ -8,7 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "858458664182",
   appId: "1:858458664182:web:1e8cde8dc062acea3710d7",
 };
-const app = initializeApp(firebaseConfig);
-// const auth = app.auth();
-const auth = getAuth(app);
-export { app, auth };
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
